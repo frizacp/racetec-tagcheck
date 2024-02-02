bbcd53ea4d0bc148d6a4a9e47adf8eb6e352203a
Dani Sumangghani <sumangghani@gmail.com>
2024-02-02 19:03:46 +0700

    Add api point

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagCheck;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::controller(TagCheck::class)->group(function () {
+    Route::get('/tagcheck', 'findall')->name('tagcheck.findall');
+    Route::patch('/tagcheck', 'update')->name('tagcheck.update');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
