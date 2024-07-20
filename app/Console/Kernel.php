906abe6cf562fdfb736e3333acdb7ae4a9fbbc7a
Dani Sumangghani <sumangghani@gmail.com>
2024-07-20 19:59:03 +0700

    Change 3 menit sekali

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('get:tias')->everyFiveMinutes();
+        $schedule->command('get:tias')->everyThreeMinutes();
     }
 
     /**
