93c5e347a8605adf677926f8f7566482e5ae6a17
Dani Sumangghani <sumangghani@gmail.com>
2024-07-20 19:59:37 +0700

    Change 2 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('get:tias')->everyThreeMinutes();
+        $schedule->command('get:tias')->everyTwoMinutes();
     }
 
     /**
