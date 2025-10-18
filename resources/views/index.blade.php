1ef6abf7b458d6191d86ded8dd4a93ad95713c9d
Friza Cahya <frizacp@gmail.com>
2025-10-18 08:32:59 +0700

    slamet_bg_dan font

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,8 +46,23 @@
 
 @section('header')
 <style>
+    @font-face {
+        font-family: 'Gotham Ultra';
+        src: url('/fonts/Gotham_Ultra.otf') format('opentype');
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'Gotham Ultra';
+        src: url('/fonts/Gotham_Ultra_Italic.ttf') format('opentype');
+        font-weight: 700;
+        font-style: italic;
+    }
+
     body {
-        background-image: url('/img/bg_white.png');
+        font-family: 'Gotham Ultra', sans-serif;
+        background-image: url('/img/bg_slamet.webp');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: top;
@@ -65,9 +80,11 @@
     }
 
     h2 {
-        font-size: 50px !important;
+        font-family: 'Gotham Ultra', sans-serif !important;
+        font-size: 80px !important;
         margin: 0 !important;
         font-weight: bold !important;
+        font-style: italic !important;
     }
 
     h3 {
@@ -83,9 +100,11 @@
     }
 
     h5 {
+        font-family: 'Gotham Ultra', sans-serif !important;
         font-size: 40px !important;
         margin: 0 !important;
         font-weight: bold !important;
+        font-style: italic;
     }
 
     h1 {
@@ -105,12 +124,11 @@
 
 
     <div class="d-flex flex-column justify-content-center align-items-center" style="min-height: 85vh;">
-        <div class="text-center pt-3">
+        <div class="text-center pt-2">
             <div class="d-flex flex-column justify-content-center align-items-center" style="min-height: 85vh;">
-
-                <h5 class="text-uppercase" id="resultname" style="color: #181818; text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);">NAME</h5>
-                <h2 class="text-uppercase" id="resultBib" style="color: #181818; text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);">BIB</h2>
-                <h5 class="text-uppercase" id="contest" style="color: #181818; text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);">CONTEST</h5>
+                <h2 class="text-uppercase" id="resultBib" style="color: #ffffff; ">BIB</h2>
+                <h5 class="text-uppercase" id="resultname" style="color: #FFFF00; ">NAME</h5>
+                <h5 class="text-uppercase" id="contest" style="color: #ffffff; ">CONTEST</h5>
             </div>
         </div>
     </div>
